ba95aa22297c8f2e86acbeb81a7477cdbf394ae9
braddabug <braddabug@80473309-fabf-408f-887a-d98baf40245d>
2009-11-06 20:08:09 +0000

    still getting ready for release
    git-svn-id: https://svn.code.sf.net/p/gk3tools/code/trunk@248 80473309-fabf-408f-887a-d98baf40245d

---
+++ sheep/create_zip.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set zip="c:\program files\7-zip\7z.exe"
+set libs="C:\Program Files (x86)\Microsoft Visual Studio 8\VC\redist\x86\Microsoft.VC80.CRT"
+set tempdir="Gk3Sheep"
+
+rem Create a temporary directory
+mkdir %tempdir%
+
+rem Create the Source directory
+mkdir %tempdir%\Source
+
+rem Copy source files into the Source directory
+copy *.cpp %tempdir%\Source
+copy *.h %tempdir%\Source
+copy *.vcproj %tempdir%\Source
+copy *.sln %tempdir%\Source
+copy *.y %tempdir%\Source
+copy *.l %tempdir%\Source
+copy makefile* %tempdir%\Source
+
+rem Copy the binary files
+copy Release\sheepc.exe %tempdir%
+copy Release\sheep.dll %tempdir%
+copy Release\sheep.lib %tempdir%
+copy *.txt %tempdir%
+
+
+rem Zip up the temporary directory
+%zip% a Sheep.zip %tempdir%\*
+
+rem Remove the temporary directory
+del /F /Q %tempdir%\Source
+del /F /Q %tempdir%
+rmdir %tempdir%\Source
+rmdir %tempdir%
