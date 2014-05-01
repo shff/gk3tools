13f34cb412d9e49819d9101644fae5f736b5c8c3
braddabug <braddabug@80473309-fabf-408f-887a-d98baf40245d>
2014-05-01 03:33:25 +0000

    oops, forgot these
    git-svn-id: https://svn.code.sf.net/p/gk3tools/code/trunk@462 80473309-fabf-408f-887a-d98baf40245d

---
+++ levelviewer/Game/Console.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    class Console : Gk3Main.Console
+    {
+        private static Console _instance;
+        private static System.Text.StringBuilder _command = new System.Text.StringBuilder();
+        private static List<string> _lines = new List<string>();
+        private static string _prevCommand;
+        private static Gk3Main.Gui.Font _font;
+        private static bool _wrap;
+        private static float _wrapWidth;
+        private static bool _visible = true;
+
+        private const int _numVisibleLines = 15;
+
+        public static Console Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Console();
+
+                return _instance;
+            }
+        }
+
+        public static bool Wrap
+        {
+            get { return _wrap; }
+            set { _wrap = value; }
+        }
+
+        public static float WrapWidth
+        {
+            get { return _wrapWidth; }
+            set { _wrapWidth = value; }
+        }
+
+        public static bool Visible
+        {
+            get { return _visible; }
+            set { _visible = value; }
+        }
+
+        public static void Load(Gk3Main.Resource.ResourceManager content)
+        {
+            _font = content.Load<Gk3Main.Gui.Font>("f_courier");
+        }
+
+        public static void Render(Gk3Main.Graphics.SpriteBatch spriteBatch)
+        {
+            if (_visible)
+            {
+                spriteBatch.Begin();
+
+                int startLine = Math.Max(0, _lines.Count - _numVisibleLines);
+
+                int cursorY = 0;
+                for (int i = startLine; i < _lines.Count; i++)
+                {
+                    _font.Print(spriteBatch, 0, cursorY, _lines[i]);
+
+                    cursorY += _font.LineHeight;
+                }
+
+                _font.Print(spriteBatch, 0, cursorY, _command.ToString());
+
+                spriteBatch.End();
+            }
+        }
+
+        public override void Write(Gk3Main.ConsoleVerbosity verbosity, string text, params object[] arg)
+        {
+            string result = string.Format(text, arg);
+
+            if (_wrap && _font != null)
+            {
+                if (_font.MeasureString(result).X > _wrapWidth)
+                    wrapLine(result);
+                else
+                    _lines.Add(string.Format(text, arg));
+            }
+            else
+            {
+                _lines.Add(string.Format(text, arg));
+            }
+            
+            System.Console.WriteLine(result);
+        }
+
+        public static void KeyPress(Keys key)
+        {
+            if (key == Keys.Up ||
+                key == Keys.Down ||
+                key == Keys.Right ||
+                key == Keys.Left)
+            {
+                ControlButtonPressed(key);
+            }
+            else
+            {
+                char c = convertKey(key);
+
+                AppendToCurrentCommand(c);
+            }
+        }
+
+        public static void AppendToCurrentCommand(char c)
+        {
+            if (c == '\b')
+            {
+                if (_command.Length > 2)
+                    _command.Remove(_command.Length - 1, 1);
+            }
+            else if (c == 10)
+            {
+                _prevCommand = _command.ToString();
+
+                _instance.Write(Gk3Main.ConsoleVerbosity.Polite, _prevCommand);
+
+                _instance.RunCommand(_prevCommand);
+
+                _command.Length = 0;
+            }
+            else if (c != 0)
+                _command.Append(c);
+        }
+
+        public static void ControlButtonPressed(Keys button)
+        {
+            if (button == Keys.Up)
+            {
+                _command.Length = 0;
+                _command.Append(_prevCommand);
+            }
+        }
+
+        private static void wrapLine(string lineToWrap)
+        {
+            string currentString = lineToWrap;
+            StringBuilder currentLine = new StringBuilder();
+            int index = 0;
+            while (true)
+            {
+                float lineWidth = _wrapWidth;
+
+                int prevSpace = -1;
+                currentLine.Length = 0;
+                while (_font.MeasureString(currentLine).X < lineWidth && index < currentString.Length)
+                {
+                    if (currentString[index] == ' ')
+                        prevSpace = currentLine.Length;
+
+                    currentLine.Append(currentString[index++]);
+                }
+
+                // uh oh, too many characters! back up until we find a space
+                if (index < currentString.Length)
+                {
+                    if (prevSpace == -1)
+                    {
+                        // no spaces found, so just break in the middle of the line
+                        string line = currentLine.ToString();
+                        line = line.Substring(0, line.Length - 1);
+                        _lines.Add(line);
+                        index--;
+                    }
+                    else
+                    {
+                        string line = currentLine.ToString().Substring(0, prevSpace);
+                        _lines.Add(line);
+                        index -= (currentLine.Length - prevSpace - 1);
+                    }
+                }
+                else
+                {
+                    // apparently we're done! add this last line and exit
+                    _lines.Add(currentLine.ToString());
+                    break;
+                }
+            }
+        }
+
+        private static char convertKey(Keys keys)
+        {
+            bool shift = Input.CurrentKeys.IsKeyDown(Keys.LeftShift) ||
+                Input.CurrentKeys.IsKeyDown(Keys.RightShift);
+
+            // A - Z
+            if (keys >= Keys.A && keys <= Keys.Z)
+            {
+                if (shift)
+                {
+                    return (char)keys;
+                }
+                else
+                {
+                    return (char)('a' + ((char)keys - 'A'));
+                }
+            }
+
+            // 0 - 9
+            else if (keys >= Keys.D0 && keys <= Keys.D9)
+            {
+                if (shift)
+                {
+                    // this assumes a standard US Querty keyboard!
+                    if (keys == Keys.D1)
+                        return '!';
+                    if (keys == Keys.D9)
+                        return '(';
+                    if (keys == Keys.D0)
+                        return ')';
+                }
+                else
+                {
+                    return (char)keys;
+                }
+            }
+            //else if (keys == Keys.OemPipe)
+            //    return '\\';
+            //else if (keys == Keys.OemOpenBrackets)
+            //    return '[';
+            //else if (keys == Keys.OemCloseBrackets)
+            //    return ']';
+
+            // special characters
+            else if (keys == Keys.Enter)
+                return '\n';
+            else if (keys == Keys.Back)
+                return '\b';
+            else if (keys == Keys.Space)
+                return ' ';
+            else if (keys == Keys.OemSemicolon)
+            {
+                if (shift)
+                    return ':';
+                else
+                    return ';';
+            }
+            else if (keys == Keys.OemPlus)
+                return '+';
+            else if (keys == Keys.OemComma)
+                return ',';
+            else if (keys == Keys.OemMinus)
+                return '-';
+            else if (keys == Keys.OemPeriod)
+            {
+                if (shift)
+                    return '>';
+                else
+                    return '.';
+            }
+            else if (keys == Keys.OemQuestion)
+            {
+                if (shift)
+                    return '?';
+                else
+                    return '/';
+            }
+            else if (keys == Keys.OemPipe)
+            {
+                if (shift)
+                    return '|';
+                else
+                    return '\\';
+            }
+            else if (keys == Keys.OemQuotes)
+            {
+                if (shift)
+                    return '"';
+                else
+                    return '\'';
+            }
+
+            return (char)0;
+        }
+    }
+}
---
+++ levelviewer/Game/Input/Keyboard.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    enum KeyState
+    {
+        Up = 0,
+        Down
+    }
+
+    struct KeyboardState
+    {
+        uint _state1;
+        uint _state2;
+        uint _state3;
+        uint _state4;
+        uint _state5;
+        uint _state6;
+        uint _state7;
+        uint _state8;
+
+        public KeyState GetState(Keys key)
+        {
+            uint stateIndex = (uint)key >> 5;
+		    if (stateIndex >= 8) return KeyState.Up;
+
+		    uint blah = (uint)1 << (int)key;
+
+            uint state = 0;
+            if (stateIndex == 0)
+                state = _state1;
+            else if (stateIndex == 1)
+                state = _state2;
+            else if (stateIndex == 2)
+                state = _state3;
+            else if (stateIndex == 3)
+                state = _state4;
+            else if (stateIndex == 4)
+                state = _state5;
+            else if (stateIndex == 5)
+                state = _state6;
+            else if (stateIndex == 6)
+                state = _state7;
+            else if (stateIndex == 7)
+                state = _state8;
+
+		    if ((blah & state) == 0)
+			    return KeyState.Up;
+
+		    return KeyState.Down;
+        }
+
+		public bool IsKeyDown(Keys key) { return GetState(key) == KeyState.Down; }
+		public bool IsKeyUp(Keys key) { return GetState(key) == KeyState.Up; }
+
+        public Keys[] GetPressedKeys()
+        {
+            List<Keys> keys = new List<Keys>();
+
+		    for (int i = 0; i < 256; i++)
+		    {
+                if (GetState((Keys)i) == KeyState.Down)
+                    keys.Add((Keys)i);
+		    }
+
+            return keys.ToArray();
+        }
+
+        public void SetState(Keys key, KeyState state)
+        {
+            uint stateIndex = (uint)key >> 5;
+
+            if (state == KeyState.Down)
+            {
+                if (stateIndex == 0)
+                    _state1 |= ((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 1)
+                    _state2 |= ((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 2)
+                    _state3 |= ((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 3)
+                    _state4 |= ((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 4)
+                    _state5 |= ((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 5)
+                    _state6 |= ((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 6)
+                    _state7 |= ((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 7)
+                    _state8 |= ((uint)(1 << (int)key) & 0xffffffff);
+            }
+            else
+            {
+                if (stateIndex == 0)
+                    _state1 &= ~((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 1)
+                    _state2 &= ~((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 2)
+                    _state3 &= ~((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 3)
+                    _state4 &= ~((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 4)
+                    _state5 &= ~((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 5)
+                    _state6 &= ~((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 6)
+                    _state7 &= ~((uint)(1 << (int)key) & 0xffffffff);
+                else if (stateIndex == 7)
+                    _state8 &= ~((uint)(1 << (int)key) & 0xffffffff);
+            }
+        }
+    }
+
+    class Keyboard
+    {
+        static KeyboardState _current;
+
+        public static KeyboardState GetState() { return _current; }
+
+        public static void UpdateFromSDLKeys(byte[] sdlkeys)
+        {
+            for (int i = 0; i < sdlkeys.Length; i++)
+                _current.SetState(translateSDLK(i), sdlkeys[i] == 0 ? KeyState.Up : KeyState.Down); 
+        }
+
+        private static Keys translateSDLK(int sdlk)
+        {
+            if (sdlk == Tao.Sdl.Sdl.SDLK_BACKQUOTE)
+                return Keys.OemTilde;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_LEFT)
+                return Keys.Left;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_UP)
+                return Keys.Up;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_RIGHT)
+                return Keys.Right;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_DOWN)
+                return Keys.Down;
+            else if (sdlk >= Tao.Sdl.Sdl.SDLK_a && sdlk <= Tao.Sdl.Sdl.SDLK_z)
+                return Keys.A + (sdlk - Tao.Sdl.Sdl.SDLK_a);
+            else if (sdlk >= Tao.Sdl.Sdl.SDLK_0 && sdlk <= Tao.Sdl.Sdl.SDLK_9)
+                return Keys.D0 + (sdlk - Tao.Sdl.Sdl.SDLK_0);
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_LSHIFT)
+                return Keys.LeftShift;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_RSHIFT)
+                return Keys.RightShift;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_SPACE)
+                return Keys.Space;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_BACKSPACE)
+                return Keys.Back;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_RETURN)
+                return Keys.Enter;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_SEMICOLON)
+                return Keys.OemSemicolon;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_PLUS)
+                return Keys.OemPlus;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_COMMA)
+                return Keys.OemComma;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_MINUS)
+                return Keys.OemMinus;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_PERIOD)
+                return Keys.OemPeriod;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_SLASH)
+                return Keys.OemQuestion;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_BACKSLASH)
+                return Keys.OemPipe;
+            else if (sdlk == Tao.Sdl.Sdl.SDLK_QUOTE)
+                return Keys.OemQuotes;
+
+            return Keys.X;
+        }
+    }
+}
